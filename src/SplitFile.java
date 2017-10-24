e80afc952d24ecf6eb04b4ccf875007bb4b1c233
Hamsika Gandamalla <hamsikag15@gmail.com>
2017-10-24 16:29:55 -0400

    Added SplitFile

---
+++ src/SplitFile.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class SplitFile {
+	private static String FILE_NAME = "C://Users//hamsi//Desktop//CNProject//src//TextFile.txt";
+	private static byte PART_SIZE = PieceSize;
+	public SplitFile(String FILE_NAME) {
+		
+		File inputFile = new File(FILE_NAME);
+		FileInputStream inputStream;
+		String newFileName;
+		FileOutputStream filePart;
+		int fileSize = (int) inputFile.length();
+		int nChunks = 0, read = 0, readLength = PART_SIZE;
+		byte[] byteChunkPart;
+		try {
+			inputStream = new FileInputStream(inputFile);
+			while (fileSize > 0) {
+				if (fileSize <= PART_SIZE) {
+					readLength = fileSize;
+				}
+				byteChunkPart = new byte[readLength];
+				read = inputStream.read(byteChunkPart, 0, readLength);
+				fileSize -= read;
+				assert (read == byteChunkPart.length);
+				nChunks++;
+				newFileName = FILE_NAME + ".part"
+						+ Integer.toString(nChunks - 1);
+				filePart = new FileOutputStream(new File(newFileName));
+				filePart.write(byteChunkPart);
+				filePart.flush();
+				filePart.close();
+				byteChunkPart = null;
+				filePart = null;
+			}
+			inputStream.close();
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}
+	}
+}
+
+
